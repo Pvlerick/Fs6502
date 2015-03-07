d761703a6d398d66e1cec8967db48db93cc251b4
Philippe <pvlerick@gmail.com>
2015-03-07 17:28:13 +0100

    Added nuspec for Emulator project

---
+++ src/Emulator/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿namespace Fs6502.Emulator
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("6502 Emulator")>]
+[<assembly: AssemblyDescription("A 6502 emulator written in F#")>]
+[<assembly: AssemblyCompany("Philippe Vlérick")>]
+[<assembly: AssemblyCopyright("Copyright ©  2014-2015")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyProduct("Fs6502")>]
+[<assembly: AssemblyTrademark("")>]
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+()
